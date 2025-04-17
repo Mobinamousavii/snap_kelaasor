6ce9d342dc42b6789f9dde46ab77bfe37d7afe77
Mobinamousavii <mobinamsv6@gmail.com>
2025-04-17 11:24:35 +0330

    idk

--- get_taxi/models.py
+++ get_taxi/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.contrib.gis.db import models
 class Wallet(models.Model):
     amount= models.BigIntegerField()
     owner= models.ForeignKey(User, on_delete= models.PROTECT, related_name= "the_owner_of_the_wallet")
