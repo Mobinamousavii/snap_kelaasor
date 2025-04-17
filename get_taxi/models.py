ce0e1a915a0e880a602ab3d549addb62bd1249c5
Mohamad Reza <m.reza.s.moghadam@gmail.com>
2025-04-17 09:21:34 +0330

    🎉 wallet class added

--- get_taxi/models.py
+++ get_taxi/models.py
@@ -1,3 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Wallet(models.Model):
+    amount= models.BigIntegerField()
+    owner= models.ForeignKey(User, on_delete= models.PROTECT, related_name= "the_owner_of_the_wallet")
