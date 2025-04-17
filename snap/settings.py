6d21022124a6577d3e61821b07a60933ce55e9c3
Mohamad Reza <m.reza.s.moghadam@gmail.com>
2025-04-17 09:48:25 +0330

    ✅ all test cases passed

--- snap/settings.py
+++ snap/settings.py
@@ -77,8 +77,13 @@ WSGI_APPLICATION = 'snap.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'snap',
+        'USER': 'postgres',
+        'PASSWORD': 'Postgresqlishere9',
+        'HOST': 'localhost',
+        'PORT': '5432'
+
     }
 }
 
